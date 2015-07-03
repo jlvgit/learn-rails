0179a8ce2b5f2ee35882c2547431fa4e3637cf9d
jlvgit <jlv44@ymail.com>
2015-07-02 21:38:13 -0400

    changed to newltter signup and added gems for heroku

--- learn-rails/app/models/contact.rb
+++ learn-rails/app/models/contact.rb
@@ -7,6 +7,6 @@ class Contact
   validates_presence_of :email
   validates_presence_of :content
   validates_format_of :email,
-  with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+    with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
 end
